e974ce4d8b36f97d74028580f2307b01edbe89a0
Abhishek <programwithabhi@gmail.com>
2024-12-03 20:00:36 +0530

    How to define a array in GO

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main(){
+  developers := [4]string{"programwithabhi", "fireship", "codewithharry", "networkchuck"}
+  fmt.Println("Those awesome 4 developers are: ", developers)
+}
