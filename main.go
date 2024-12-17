6b556d38b30e77e443cc59c72e964fc0fe86fcd5
Abhishek <programwithabhi@gmail.com>
2024-12-17 09:33:20 +0530

    Select slice from slice and also with start to specified index also you can left the last element empty that's how you can select from specified element to the end of the array or slice

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import "fmt"
 func main() {
 	prices := [4]float64{11.2, 22.0, 23.3, 12.4}
 	fmt.Println("prices are: ", prices)
+
 	slicedPrices := prices[1:3]
 	fmt.Println("slicesPrices are: ", slicedPrices)
+
+	highlightedPrice := slicedPrices[:1] // Select the slice from start to specified index from an array and exclude the last specified indexed element
+	fmt.Println("Highlighted price is: ", highlightedPrice)
 }
