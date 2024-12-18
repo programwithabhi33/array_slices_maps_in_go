b0ecd254f8e0766d88090b2922393fbf7328be7c
Abhishek <programwithabhi@gmail.com>
2024-12-18 09:49:01 +0530

    When you create slice from the original array then the slice is actually pointing the original array (copy not created) and the capacity of the slice is calculated on how many element's available on the right side of the original array relative to the current slice starting element

--- main.go
+++ main.go
@@ -9,6 +9,9 @@ func main() {
 	slicedPrices := prices[1:3]
 	fmt.Println("slicesPrices are: ", slicedPrices)
 
-	highlightedPrice := slicedPrices[:1] // Select the slice from start to specified index from an array and exclude the last specified indexed element
+	highlightedPrice := slicedPrices[1:2] // Select the slice from start to specified index from an array and exclude the last specified indexed element
 	fmt.Println("Highlighted price is: ", highlightedPrice)
+	fmt.Println("The length of the highlighted price is: ", len(highlightedPrice))
+	fmt.Println("The capacity of the highlighted price is: ", cap(highlightedPrice))
+
 }
